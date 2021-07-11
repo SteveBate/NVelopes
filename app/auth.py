512caf170413bff4e07eb27c6e34865006299434
Steve Bate <stephencbate@gmail.com>
2021-07-11 08:01:09 +0100

    Moved secrets to enviroment variables like a good boy If this was a production system we'd not store these in source control

--- app/auth.py
+++ app/auth.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime, timedelta
 from typing import Optional
 from fastapi import Depends, HTTPException, status
@@ -11,11 +12,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-# openssl rand -hex 32
-SECRET_KEY = "0c8eaab3eba38db00e17358b08dd28425b14561f2ec174a4b75ee3c566e92e15"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
+# generate a secret key using openssl rand -hex 32
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
@@ -42,7 +39,7 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     else:
         expire = datetime.utcnow() + timedelta(minutes=15)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, os.environ['JWT_SECRET_KEY'], algorithm=os.environ['JWT_ALGORITHM'])
     return encoded_jwt
 
 
@@ -54,7 +51,7 @@ def __get_current_user(token: str = Depends(oauth2_scheme)):
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, os.environ['JWT_SECRET_KEY'], algorithms=[os.environ['JWT_ALGORITHM']])
         username: str = payload.get("sub")
         if username is None: raise credentials_exception
         token_data = TokenData(username=username)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 load_dotenv()  # take environment variables from .env.
 
+import os
 import app.auth as auth
 from datetime import timedelta
 from fastapi import Depends, HTTPException, FastAPI, status
@@ -211,6 +212,6 @@ def create_new_user(req: NewUserRequest):
 def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
     user = auth.authenticate_user(db, form_data.username, form_data.password)
     if not user: raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password", headers={"WWW-Authenticate": "Bearer"},)
-    access_token_expires = timedelta(minutes=auth.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token_expires = timedelta(minutes=float(os.environ['JWT_EXPIRES']))
     access_token = auth.create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
     return {"access_token": access_token, "token_type": "bearer"}
